8047b2784cca3eb1d70f3e9d3a4fe710eca08df2
Devan Adhia <adhia@usc.edu>
2019-07-10 13:34:31 -0400

    add card selectability to UI API; added mouse click event listener; added functionality to update statuses and show usernames

--- static/console-websocket-game.js
+++ static/console-websocket-game.js
@@ -1,38 +1,50 @@
 var canvas;
 var cardsRemainingText;
 var cardsDiscardedText;
+var attackingStatusText, defendingStatusText, addingStatusText;
 var cards = {};
-var deckLocation = generateLocation(170, 330);
-var trumpLocation = generateLocation(220, 330);
-var discardLocation= generateLocation(440, 330);
-var attackSquareLocations = new Array(6);
-var defenseSquareLocations = new Array(6);
-var chairLocations = [generateLocation(430, 95), generateLocation(675, 15), generateLocation(890, 95),
+const deckLocation = generateLocation(170, 330);
+const trumpLocation = generateLocation(220, 330);
+const discardLocation= generateLocation(440, 330);
+
+const chairLocations = [generateLocation(430, 95), generateLocation(675, 15), generateLocation(890, 95),
     generateLocation(890, 410), generateLocation(675, 505), generateLocation(430, 410)];
-var playerIndices = [[1, 4], [0, 2, 4], [0, 2, 3, 5], [0, 1, 2, 3, 5], [0, 1, 2, 3, 4, 5]];
+const usernameLocations = [generateLocation(280, 90), generateLocation(640, 10), generateLocation(960, 95),
+    generateLocation(960, 410), generateLocation(640, 510), generateLocation(280, 410)];
+const statusTextLocations = [generateLocation(280, 105), generateLocation(640, 25), generateLocation(960, 110),
+    generateLocation(960, 425), generateLocation(640, 525), generateLocation(280, 425)];
+const playerIndices = [[1, 4], [0, 2, 4], [0, 2, 3, 5], [0, 1, 2, 3, 5], [0, 1, 2, 3, 4, 5]];
 var trumpCard;
 
 var firstCardLocation = generateLocation(80, 630);
 
+var attackSquareLocations = new Array(6);
+var defenseSquareLocations = new Array(6);
 for (let i = 0; i < 6; i++) {
     let leftCoordinate = 535 + (i * 60);
     attackSquareLocations[i] = generateLocation(leftCoordinate, 160);
     defenseSquareLocations[i] = generateLocation(leftCoordinate, 240);
 }
 
+var cardsDrawnInHand = [];
+
+var objectToCard = {};
+var objectToSquare = {};
+
+var attackSquares = new Array(6);
+var defenseSquares = new Array(6);
+
 function generateBackgroundGraphics() {
-    var background = generateRect(generateLocation(0, 0), '#004C00', 1400, 600);
-    var handBackground = generateRect(generateLocation(0, 600), '#BE9B7B', 1400, 200);
+    var background = generateRect(generateLocation(0, 0), '#004C00', 1400, 600, String('background'));
+    var handBackground = generateRect(generateLocation(0, 600), '#BE9B7B', 1400, 200, String('background'));
     var table = generateEllipse(generateLocation(700, 300), 'black', '#BE9B7B', 250, 200);
-    var attackSquares = new Array(6);
-    var defenseSquares = new Array(6);
     for (let i = 0; i < 6; i++) {
-        attackSquares[i] = generateRect(attackSquareLocations[i], '#4C814C', 40, 56);
-        defenseSquares[i] = generateRect(defenseSquareLocations[i],'#4C814C', 40, 56);
+        attackSquares[i] = generateRect(attackSquareLocations[i], '#4C814C', 40, 56, 'attack' + i);
+        defenseSquares[i] = generateRect(defenseSquareLocations[i],'#4C814C', 40, 56, 'defense' + i);
     }
-    var attackText = generateTextObject('Attack', generateLocation(680, 120), 20);
-    var defenseText = generateTextObject('Defense', generateLocation(680, 300), 20);
-    var yourHandText = generateTextObject('Your Hand:', generateLocation(0, 600), 20);
+    var attackText = generateTextObject('Attack', generateLocation(680, 120), 20, '#A40B0B');
+    var defenseText = generateTextObject('Defense', generateLocation(680, 300), 20, '#A40B0B');
+    var yourHandText = generateTextObject('Your Hand:', generateLocation(0, 600), 20, '#A40B0B');
     canvas.add(background, table);
     canvas.add(handBackground);
     for (let i = 0; i < 6; i ++) {
@@ -40,6 +52,47 @@ function generateBackgroundGraphics() {
     }
     canvas.add(attackText, defenseText);
     canvas.add(yourHandText);
+}
+
+function generateUsernameTexts(numPlayers, names) {
+    for (let i = 0; i < numPlayers; i++) {
+        const location = usernameLocations[playerIndices[numPlayers-2][i]];
+        const text = generateTextObject(names[i], location, 12, '#f4f6f8');
+        canvas.add(text);
+    }
+}
+
+function generatePlayersStatusTexts() {
+    attackingStatusText = generateTextObject('Attacking', generateLocation(0,0), 12, '#FBA92E');
+    defendingStatusText = generateTextObject('Defending', generateLocation(0, 0), 12, '#FBA92E');
+    addingStatusText = generateTextObject('Adding', generateLocation(0, 0), 12, '#FBA92E');
+}
+
+function drawAttacking(numPlayers, playerNo) {
+    attackingStatusText.set(statusTextLocations[playerIndices[numPlayers-2][playerNo]]);
+    canvas.add(attackingStatusText);
+}
+
+function drawDefending(numPlayers, playerNo) {
+    defendingStatusText.set(statusTextLocations[playerIndices[numPlayers-2][playerNo]]);
+    canvas.add(defendingStatusText);
+}
+
+function drawAdding(numPlayers, playerNo) {
+    addingStatusText.set(statusTextLocations[playerIndices[numPlayers-2][playerNo]]);
+    canvas.add(addingStatusText);
+}
+
+function eraseAttacking() {
+    canvas.remove(attackingStatusText);
+}
+
+function eraseDefending() {
+    canvas.remove(defendingStatusText);
+}
+
+function eraseAdding() {
+    canvas.remove(addingStatusText);
 }
 
 function generateChairs(numPlayers) {
@@ -79,33 +132,34 @@ function generateEllipse(location, stroke, fill, rx, ry) {
     });
 }
 
-function generateRect(location, fill, width, height) {
+function generateRect(location, fill, width, height, id) {
     return new fabric.Rect({
         left: location.left,
         top: location.top,
         fill: fill,
         width: width,
         height: height,
-        selectable: false
+        selectable: false,
+        id: id
     });
 }
 
-function generateTextObject(text, location, fontSize) {
+function generateTextObject(text, location, fontSize, fill) {
     return new fabric.Text(text, {
         fontFamily: 'sans-serif',
         left: location.left,
         top: location.top,
         fontSize: fontSize,
         textAlign: 'left',
-        fill:'#A40B0B',
+        fill: fill,
         selectable: false
     });
 }
 
 function drawStatusText() {
-    cardsRemainingText = generateTextObject('Cards Remaining: 00', generateLocation(510, 390), 12);
+    cardsRemainingText = generateTextObject('Cards Remaining: 00', generateLocation(510, 390), 12, '#A40B0B');
     canvas.add(cardsRemainingText);
-    cardsDiscardedText = generateTextObject('Cards Discarded: 00', generateLocation(750, 390), 12);
+    cardsDiscardedText = generateTextObject('Cards Discarded: 00', generateLocation(750, 390), 12, '#A40B0B');
     canvas.add(cardsDiscardedText);
 }
 
@@ -115,7 +169,7 @@ function updateCardsRemaining(numCards) {
 
 function generateCardObject(id) {
     var image = document.getElementById('card-' + id);
-    var cardCanvasObject = new fabric.Image(image, {selectable: false});
+    var cardCanvasObject = new fabric.Image(image, {selectable: false, id:id});
     cardCanvasObject.scale(0.3);
     return cardCanvasObject;
 }
@@ -127,14 +181,18 @@ function pregenerateCardObjects() {
         for (let i = 2; i < 11; i ++) {
             let cardString = String(suits.charAt(suitIndex)) + String(i);
             cards[cardString] = generateCardObject(cardString);
+            objectToCard[cards[cardString]] = cardString;
         }
         for (let i = 0; i < royals.length; i++) {
             let cardString = String(suits.charAt(suitIndex)) + String(royals.charAt(i));
             cards[cardString] = generateCardObject(cardString);
+            objectToCard[cards[cardString]] = cardString;
         }
     }
     cards['deck'] = generateCardObject('back');
     cards['discard'] = generateCardObject('back');
+    objectToCard[cards['discard']] = 'discard';
+    objectToCard[cards['deck']] = 'deck';
 }
 
 function drawCard(card, location) {
@@ -179,7 +237,7 @@ function prepareCanvas(numPlayers) {
     drawStatusText();
 }
 
-function placeCardInHand(card, position) {
+function drawCardInHand(card, position) {
     let left = firstCardLocation.left + (position * 42);
     let top = firstCardLocation.top;
     if (position > 25) {
@@ -187,17 +245,42 @@ function placeCardInHand(card, position) {
         left -= 26 * 42;
     }
     const location = generateLocation(left, top);
-    console.log(location);
     drawCard(card, location);
+    cardsDrawnInHand.push(cards[card]);
+}
+
+function makeCardUnselectable(card) {
+    cards[card].set({opacity: 0.4});
+}
+
+function makeCardSelectable(card) {
+    cards[card].set({opacity: 1.0});
 }
 
 $(document).ready(function() {
     prepareCanvas(6);
-    placeCardInHand('ca', 0);
-    placeCardInHand('c2', 1);
-    placeCardInHand('da', 10);
-    placeCardInHand('d10', 25);
-    placeCardInHand('d9', 26);
-    placeCardInHand('d3', 27);
-    placeCardInHand('d4', 51);
+    drawCardInHand('ca', 0);
+    drawCardInHand('c2', 1);
+    drawCardInHand('da', 10);
+    drawCardInHand('d10', 25);
+    drawCardInHand('d9', 26);
+    drawCardInHand('d3', 27);
+    drawCardInHand('d4', 51);
+    eraseCard('d4');
+    generateUsernameTexts(6, ['devan@adhiafunds.com', 'devan@adhiafunds.com', 'devan@adhiafunds.com',
+    'devan@adhiafunds.com', 'devan@adhiafunds.com', 'devan@adhiafunds.com']);
+    generatePlayersStatusTexts();
+    drawAttacking(6, 1);
+    drawDefending(6, 2);
+    drawAdding(6, 0);
+
+    canvas.on('mouse:down', function(options) {
+        if (options.target) {
+            let type = options.target.type;
+            if (type === 'image' || type === 'rect') {
+                console.log(options.target.id);
+            }
+        }
+    });
+
 });
